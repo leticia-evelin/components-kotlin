f4cdbe564908fe55ac98ea3763f04d66280be3ab
LETÍCIA EVELIN AGUIAR E SILVA <22282118@dsn127m1246727.cfp127aluno.local>
2023-07-28 09:23:09 -0300

    inicio componentes

---
+++ app/src/main/java/br/senai/sp/jandira/componentes/login/screen/LoginScreen.kt
@@ -0,0 +1,27 @@
+package br.senai.sp.jandira.componentes.login.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.componentes.login.components.Header
+
+@Composable
+fun LoginScreen() {
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.Blue)
+    ) {
+        Header()
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
